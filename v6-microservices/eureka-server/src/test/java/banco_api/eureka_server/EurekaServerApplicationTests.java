4063d13f5617ad2f8639a628b6e2f609ae63d07e
Isaías <isaias1234t@gmail.com>
2026-06-21 11:16:18 -0300

    feat: eureka server - service discovery configurado na porta 8761

---
+++ v6-microservices/eureka-server/src/test/java/banco_api/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package banco_api.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
