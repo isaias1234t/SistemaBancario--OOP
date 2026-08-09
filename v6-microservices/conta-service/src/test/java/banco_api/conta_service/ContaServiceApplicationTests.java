5083e7b39a5b5fb1dcc7b4c7db23417f0c48dcf6
Isaías <isaias1234t@gmail.com>
2026-08-09 13:34:11 -0300

    Correção comentários

--- v6-microservices/conta-service/src/test/java/banco_api/conta_service/ContaServiceApplicationTests.java
+++ v6-microservices/conta-service/src/test/java/banco_api/conta_service/ContaServiceApplicationTests.java
@@ -3,7 +3,7 @@ package banco_api.conta_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+//Teste contexto
 @SpringBootTest
 class ContaServiceApplicationTests {
 
