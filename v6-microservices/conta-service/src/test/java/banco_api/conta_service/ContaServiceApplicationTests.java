41ee9d98f24fde44c85b569305bc75b3d12a3779
Isaías <isaias1234t@gmail.com>
2026-08-09 13:45:09 -0300

    Correção import TestPropertySource

--- v6-microservices/conta-service/src/test/java/banco_api/conta_service/ContaServiceApplicationTests.java
+++ v6-microservices/conta-service/src/test/java/banco_api/conta_service/ContaServiceApplicationTests.java
@@ -2,9 +2,11 @@ package banco_api.conta_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 //Teste contexto
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ContaServiceApplicationTests {
 
 	@Test
